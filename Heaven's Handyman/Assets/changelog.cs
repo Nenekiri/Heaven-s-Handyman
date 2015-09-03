2ae8a6e860b649acd53c4f8024c694d72dce13e9
Nenekiri <MJM16@pct.edu>
2015-09-03 14:05:12 -0400

    added the 2d toolkit asset and made a very simple scene.

--- Heaven's Handyman/Assets/changelog.cs
+++ Heaven's Handyman/Assets/changelog.cs
@@ -3,20 +3,23 @@ using System.Collections;
 
 public class changelog : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	/*This is a script to keep track of all the changes that are made to 
+	the project over the course of the semester. 
+
+	9/3/15: 
+
+	started the process of working on my Capstone Project for the Fall 2015 semester. 
+	Super excited for this, hopefully that excitement will last. 
+
+	changes made: 
+	created a new repository on GitHub that will serve as my way of backing things up 
+	and keeping changes made to the project current
+
+	made it so that I can commit changes to the repository from my local computer
+
+	imported 2d Toolkit asset package
+
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-
-	
-	}
-
-	//This is a script to keep track of all the changes that are made to 
-	//the project over the course of the semester. 
-
-
+	*/
 }
