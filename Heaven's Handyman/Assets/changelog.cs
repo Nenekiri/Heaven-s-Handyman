785c3a9e058dcf4c213f60aa76b303f759c0c394
Nenekiri <MJM16@pct.edu>
2015-09-03 12:35:00 -0400

    I'm sometimes a dumbass, needed to close project in order to commit.

---
+++ Heaven's Handyman/Assets/changelog.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changelog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	//This is a script to keep track of all the changes that are made to 
+	//the project over the course of the semester. 
+
+
+}
