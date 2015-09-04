7a6f8a63ef5fef24e12abbdd14ecf309505cd647
Nenekiri <MJM16@pct.edu>
2015-09-03 23:53:03 -0400

    basis of movement system is down, will have to tweak extensively

--- Heaven's Handyman/Assets/followCam.cs
+++ Heaven's Handyman/Assets/followCam.cs
@@ -3,18 +3,34 @@ using System.Collections;
 
 public class followCam : MonoBehaviour {
 
-	public float dampTime = 0.25f;
-	private Vector3 velocity = Vector3.zero;
-	public Transform target;
+	public float interpVelocity;
+	public float minDistance;
+	public float followDistance;
+	public GameObject target;
+	public Vector3 offset;
+	Vector3 targetPos;
+	// Use this for initialization
+	void Start () {
+		targetPos = transform.position;
+	}
 	
-	void Update () 
-	{
-		if (target.position.x > 0)
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (target)
 		{
-			Vector3 point = camera.WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.3f, 0.4f, point.z));
-			Vector3 destination = transform.position + delta;
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+			
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+			
+			interpVelocity = targetDirection.magnitude * 5f;
+			
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
+			
+			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
+			
 		}
 	}
-}
+
+
+}//end of update
--- Heaven's Handyman/Assets/playerControls.cs
+++ Heaven's Handyman/Assets/playerControls.cs
@@ -5,11 +5,20 @@ public class playerControls : MonoBehaviour {
 
 	public float speed = 1.0f;
 	public float JumpSpeed = 2.0f; 
+	public float fallSpeed = 2.5f; 
+
+	public float ascentTime = 0.0f; 
+
+	public Rigidbody2D rb; 
 
 	// Use this for initialization
 	void Start () {
+
+		rb = GetComponent<Rigidbody2D>(); 
 	
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,11 +32,41 @@ public class playerControls : MonoBehaviour {
 		var move = new Vector3(Input.GetAxis("Horizontal"),0, 0);
 		transform.position += move * speed * Time.deltaTime;
 
-		//this method allows for jumping... a lot
+	
+		if(Input.GetButtonUp("Jump") == true){
+
+			//rb.velocity = new Vector3 (0, 10, 0); 
+
+			var moveVertTemp = new Vector3(0, JumpSpeed, 0); 
+			transform.position += moveVertTemp * Time.deltaTime;
+
+		} 
+
+
+
+		//this method allows for jumping and holding down the jump key to ascend
 		var moveVert = new Vector3(0, Input.GetAxis("Jump"), 0); 
-		transform.position += moveVert * JumpSpeed * Time.deltaTime; 
+			transform.position += moveVert * JumpSpeed * Time.deltaTime; 
+
+
+
+
+		//used for quickly dropping down
+
+		if(Input.GetAxis("Vertical") < 0){
+
+			var moving = new Vector3(0, fallSpeed, 0); 
+			transform.position -= moving * Time.deltaTime; 
+
+
+		}
+
+		//much less effective way of dropping the character
+		//var moveDown = new Vector3(0, Input.GetAxis("Vertical"), 0); 
+		//transform.position -= moveDown * fallSpeed * Time.deltaTime;  
 
 
 	
-	}
-}
+	}//end of update
+
+}//end of class
